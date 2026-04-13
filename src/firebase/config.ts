2c8841bbf3bb2659bf74cf1bb810934b0822f3e5
manvika ramesha <manvikaramesha@gmail.com>
2026-04-13 05:52:09 +0000

    Try fixing this error: `Runtime FirebaseError: Firebase Storage: No defa

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:591426031661:web:2b652fc206fb02d6cd68a4",
   "apiKey": "AIzaSyAwwcvTpPWedG-Kenu435j_VYdl5trjiYw",
   "authDomain": "studio-770907852-54c9e.firebaseapp.com",
+  "storageBucket": "studio-770907852-54c9e.firebasestorage.app",
   "measurementId": "",
   "messagingSenderId": "591426031661"
 };
